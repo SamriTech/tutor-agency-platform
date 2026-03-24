d9041824bb97be7fb3dc52b93782324aba4cbedb
natiman52 <natnaelyazchew135210@gmail.com>
2026-03-25 01:08:24 +0300

    MVP production fix 6

--- backend/ey_backend/settings.py
+++ backend/ey_backend/settings.py
@@ -14,12 +14,12 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 from datetime import timedelta
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+load_dotenv(Path.joinpath(BASE_DIR,".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
