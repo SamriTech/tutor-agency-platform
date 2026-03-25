0910de0c5024198dd5d9e8b923c9a560337cfb7c
natiman52 <natnaelyazchew135210@gmail.com>
2026-03-25 10:09:23 +0300

    MVP Production 11

--- backend/user/views.py
+++ backend/user/views.py
@@ -233,7 +233,7 @@ class DepositView(GenericAPIView):
             last_name=user.last_name or "",
             tx_ref=tx_ref,
             callback_url=request.build_absolute_uri("/api/auth/wallet/webhook/"),
-            return_url="http://localhost:3000/wallet/history"
+            return_url=request.build_absolute_uri("/wallet/history")
         )
         
         if res.get("status") == "success":
