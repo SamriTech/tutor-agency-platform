8e0bf195605b1a94dd895199c2a7829f66384dd6
natiman52 <natnaelyazchew135210@gmail.com>
2026-05-15 21:23:06 +0300

    fixed env

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,7 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      "process.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL) || JSON.stringify("http://127.0.0.1:8000"),
+      "process.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
     },
     resolve: {
       alias: {
